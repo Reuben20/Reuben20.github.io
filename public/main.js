618878c74d7f34c5bc077f6594c485520b63e221
James <rubencamcalderon@gmail.com>
2022-02-18 16:31:24 -0500

    korean language added

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const languages = ["進行中の作業...","Trabajo en progreso...","Work in progress..."]
+const languages = ["進行中の作業...","Trabajo en progreso...","Work in progress...","진행중인 작업..."]
 var elemento = document.getElementById('titulo')
 let counter = 0
 let now = new Date()
@@ -6,7 +6,7 @@ let now = new Date()
 const change_text = () => {
   elemento.textContent = languages[counter]
   counter++
-  if(counter === 3) {counter = 0}
+  if(counter === 4) {counter = 0}
 }
 
 if(now.getHours() < 19){
